866719f31b622cb2cca1699aa2c67e56ca1a6cbb
szymonguzik <szymon.guzik@microsoft.wsei.edu.pl>
2021-10-02 14:19:44 +0200

    Init app

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace repo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikacja emerytura");
+            Console.WriteLine("Podaj swoje imie");
+            string name = Console.ReadLine();;
+            
+            Console.WriteLine("Twoje imie to {0}", name);
+        }
+    }
+}
